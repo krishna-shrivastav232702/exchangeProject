88a2e788679ac990a29d1ad5fbc204ff6f3864d5
krishna <krishnashrivastava23@gmail.com>
2025-05-24 23:59:00 +0530

    feat: enhance order processing with cancellation and depth updates

--- exchange-backend/engine/src/trade/Engine.ts
+++ exchange-backend/engine/src/trade/Engine.ts
@@ -1,5 +1,6 @@
 import { RedisManager } from "../RedisManager";
-import { CREATE_ORDER, MessageFromApi } from "../types/fromApi";
+import { ORDER_UPDATE } from "../types";
+import { CANCEL_ORDER, CREATE_ORDER, MessageFromApi } from "../types/fromApi";
 import { Fill, Order, Orderbook } from "./Orderbook";
 import dotenv from "dotenv"
 import fs from "fs"
@@ -65,11 +66,95 @@ export class Engine {
         switch (message.type) {
             case CREATE_ORDER:
                 try {
-                    this.createOrder(message.data.market, message.data.price, message.data.quantity, message.data.side, message.data.userId);
+                    const { executedQty, fills, orderId } = this.createOrder(message.data.market, message.data.price, message.data.quantity, message.data.side, message.data.userId);
+                    RedisManager.getInstance().sendToApi(clientId, {
+                        type: "ORDER_PLACED",
+                        payload: {
+                            orderId,
+                            executedQty,
+                            fills
+                        }
+                    });
                 } catch (error) {
-
+                    console.log(error);
+                    RedisManager.getInstance().sendToApi(clientId, {
+                        type: "ORDER_CANCELLED",
+                        payload: {
+                            executedQty: 0,
+                            orderId: "",
+                            remainingQty: 0
+                        }
+                    })
                 }
+                break;
+            case CANCEL_ORDER:
+                try{
+                    const orderId = message.data.orderId;
+                    const cancelMarket = message.data.market;
+                    const cancelOrderbook = this.orderbooks.find(o => o.ticker() === cancelMarket);
+                    const quoteAsset = cancelMarket.split("_")[1];
+                    if(!cancelOrderbook){
+                        throw new Error("No orderbook found");
+                    }
+
+                    const order = cancelOrderbook.asks.find(o => o.orderId === orderId) || cancelOrderbook.bids.find(o => o.orderId === orderId);
+                    if(!order){
+                        console.log("no order found");
+                        throw new Error("No order found");
+                    }
+                    if(order.side === "buy"){
+                        const price = cancelOrderbook.cancelBid(order);
+                        const leftQuantity = (order.quantity - order.filled) * order.price;
+                        //@ts-ignore
+                        this.balances.get(order.userId)[BASE_CURRENCY].available += leftQuantity;
+                        //@ts-ignore
+                        this.balances.get(order.userId)[BASE_CURRENCY].locked -= leftQuantity;
+                        if(price){
+                            this.sendUpdatedDepthAt(price.toString(),cancelMarket)
+                        }
+                    }else{
+                        const price = cancelOrderbook.cancelAsk(order);
+                        const leftQuantity = order.quantity - order.filled;
+                        //@ts-ignore
+                        this.balances.get(order.userId)[quoteAsset].available += leftQuantity;
+                        //@ts-ignore
+                        this.balances.get(order.userId)[quoteAsset].locked -= leftQuantity;
+                        if(price){
+                            this.sendUpdatedDepthAt(price.toString(),cancelMarket);
+                        }
+                    }
+                    RedisManager.getInstance().sendToApi(clientId,{
+                        type:"ORDER_CANCELLED",
+                        payload:{
+                            orderId,
+                            executedQty:0,
+                            remainingQty:0
+                        }
+                    })
+                }catch(error){
+                    console.log("error while cancelling order");
+                    console.log(error);
+                }
+                break;
         }
+    }
+
+    sendUpdatedDepthAt(price:string,market:string){
+        const orderbook = this.orderbooks.find(o => o.ticker() === market);
+        if(!orderbook){
+            return;
+        }
+        const depth = orderbook.getDepth();
+        const updatedBids = depth?.bids.filter(x => x[0]===price);
+        const updatedAsks = depth?.asks.filter(x => x[0] === price);
+        RedisManager.getInstance().publishMessage(`depth@${market}`,{
+            stream:`depth@${market}`,
+            data:{
+                a: updatedAsks.length ? updatedAsks : [[price,"0"]],
+                b: updatedAsks.length ? updatedBids : [[price,"0"]],
+                e: "depth"
+            }
+        });
     }
 
     createOrder(market: string, price: string, quantity: string, side: "buy" | "sell", userId: string) {
@@ -81,27 +166,126 @@ export class Engine {
         }
         this.checkAndLockFunds(baseAsset, quoteAsset, side, userId, quoteAsset, price, quantity);
 
-        const order:Order ={
-            price:Number(price),
-            quantity:Number(quantity),
-            orderId: Math.random().toString(36).substring(2,15)+Math.random().toString(36).substring(2,15),
-            filled:0,
+        const order: Order = {
+            price: Number(price),
+            quantity: Number(quantity),
+            orderId: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+            filled: 0,
             side,
             userId
         }
 
-        const {fills,executedQty} = orderbook.addOrder(order);
-        this.updateBalance(userId,baseAsset,quoteAsset,side,fills,executedQty);
-
-
+        const { fills, executedQty } = orderbook.addOrder(order);
+        this.updateBalance(userId, baseAsset, quoteAsset, side, fills, executedQty);
+        this.createDbTraders(fills, market, userId);
+        this.updateDbOrders(order, executedQty, fills, market);
+        this.publishWsDepthUpdates(fills, price, side, market);
+        this.publishWsTrades(fills, userId, market);
+        return {
+            executedQty,
+            fills,
+            orderId: order.orderId
+        };
     }
 
-    updateBalance(userId:string,baseAsset:string,quoteAsset:string,side:"buy"|"sell",fills:Fill[],executedQty:number){
-        if(side==="buy"){
+    publishWsTrades(fills: Fill[], userId: string, market: string) {
+        fills.forEach(fill => {
+            RedisManager.getInstance().publishMessage(`trade@${market}`, {
+                stream: `trade@${market}`,
+                data: {
+                    e: "trade",
+                    t: fill.tradeId,
+                    m: fill.otherUserId === userId,
+                    p: fill.price,
+                    q: fill.qty.toString(),
+                    s: market
+                }
+            })
+        })
+    }
+
+
+    publishWsDepthUpdates(fills: Fill[], price: string, side: "buy" | "sell", market: string) {
+        const orderbook = this.orderbooks.find(o => o.ticker() === market);
+        if (!orderbook) {
+            return;
+        }
+        const depth = orderbook.getDepth();
+        if (side === "buy") {
+            const updatedAsks = depth?.asks.filter(x => fills.map(f => f.price).includes(x[0].toString()));
+            const updatedBid = depth?.bids.find(x => x[0] === price);
+            console.log("publish ws depth updates");
+            RedisManager.getInstance().publishMessage(`depth@${market}`, {
+                stream: `depth@${market}`,
+                data: {
+                    a: updatedAsks,
+                    b: updatedBid ? [updatedBid] : [],
+                    e: "depth"
+                }
+            });
+        }
+        if (side === "sell") {
+            const updatedBids = depth?.bids.filter(x => fills.map(f => f.price).includes(x[0].toString()));
+            const updatedAsk = depth?.asks.find(x => x[0] === price);
+            console.log("Publish ws depth updates");
+            RedisManager.getInstance().publishMessage(`depth@${market}`, {
+                stream: `depth@${market}`,
+                data: {
+                    a: updatedAsk ? [updatedAsk] : [],
+                    b: updatedBids,
+                    e: "depth"
+                }
+            })
+        }
+    }
+
+    updateDbOrders(order: Order, executedQty: number, fills: Fill[], market: string) {
+        RedisManager.getInstance().pushMessage({
+            type: ORDER_UPDATE,
+            data: {
+                orderId: order.orderId,
+                executedQty: executedQty,
+                market: market,
+                price: order.price.toString(),
+                quantity: order.quantity.toString(),
+                side: order.side,
+            }
+        })
+
+        fills.forEach(fill => {
+            RedisManager.getInstance().pushMessage({
+                type: "ORDER_UPDATE",
+                data: {
+                    orderId: fill.markerOrderId,
+                    executedQty: fill.qty
+                }
+            })
+        })
+    }
+
+    createDbTraders(fills: Fill[], market: string, userId: string) {
+        fills.forEach(fill => {
+            RedisManager.getInstance().pushMessage({
+                type: "TRADE_ADDED",
+                data: {
+                    market: market,
+                    id: fill.tradeId.toString(),
+                    isBuyerMaker: fill.otherUserId === userId,
+                    price: fill.price,
+                    quantity: fill.qty.toString(),
+                    quoteQuantity: (fill.qty * Number(fill.price)).toString(),
+                    timestamp: Date.now()
+                }
+            })
+        })
+    }
+
+    updateBalance(userId: string, baseAsset: string, quoteAsset: string, side: "buy" | "sell", fills: Fill[], executedQty: number) {
+        if (side === "buy") {
             fills.forEach(fill => {
                 // update quote asset balance
                 //@ts-ignore
-                this.balances.get(fill.otherUserId)[quoteAsset].available = this.balances.get(fill.otherUserId)?.[quoteAsset].available + (fill.qty*fill.price);
+                this.balances.get(fill.otherUserId)[quoteAsset].available = this.balances.get(fill.otherUserId)?.[quoteAsset].available + (fill.qty * fill.price);
 
                 //@ts-ignore
                 this.balances.get(userId)[quoteAsset].locked = this.balances.get(userId)?.[quoteAsset].locked - (fill.qty * fill.price);
@@ -113,11 +297,11 @@ export class Engine {
                 this.balances.get(userId)[baseAsset].available = this.balances.get(userId)?.[baseAsset].available + fill.qty;
 
             });
-        }else{
+        } else {
             fills.forEach(fill => {
                 // update quote asset
                 //@ts-ignore
-                this.balances.get(fill.otherUserId)[quoteAsset].locked = this.balances.get(fill.otherUserId)?.[quoteAsset].locked - (fill.qty*fill.price);
+                this.balances.get(fill.otherUserId)[quoteAsset].locked = this.balances.get(fill.otherUserId)?.[quoteAsset].locked - (fill.qty * fill.price);
 
                 //@ts-ignore
                 this.balances.get(userId)[quoteAsset].available = this.balances.get(userId)?.[quoteAsset].available + (fill.qty * fill.price);
--- exchange-backend/engine/src/types/toApi.ts
+++ exchange-backend/engine/src/types/toApi.ts
@@ -1,9 +1,30 @@
+import { Order } from "../trade/Orderbook"
+
 export type MessageToApi = {
-    type:"DEPTH",
-    payload:{
-        bids:[string,string][], //array of tuple , each tuple -> two strings ( price , quantity)
-        asks:[string,string][],
+    type: "DEPTH",
+    payload: {
+        bids: [string, string][], //array of tuple , each tuple -> two strings ( price , quantity)
+        asks: [string, string][],
     }
 } | {
-
+    type:"ORDER_PLACED",
+    payload:{
+        orderId: string,
+        executedQty: number,
+        fills:{
+            price:string,
+            qty:number,
+            tradeId: number
+        }[]
+    }
+} | {
+    type:"ORDER_CANCELLED",
+    payload:{
+        orderId:string,
+        executedQty: number,
+        remainingQty: number
+    }
+} | {
+    type:"OPEN_ORDERS",
+    payload:Order[]
 }
