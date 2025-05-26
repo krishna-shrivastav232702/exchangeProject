dfa579a15a233774f536194456ae901ca6afac79
krishna <krishnashrivastava23@gmail.com>
2025-05-26 22:56:44 +0530

    feat: implement order and depth routes with Redis integration, add dotenv for environment variables

--- exchange-backend/api/src/index.ts
+++ exchange-backend/api/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import { orderRouter } from "./routes/order";
+import { depthRouter } from "./routes/depth";
+import { klineRouter } from "./routes/kline";
 
 
 const app = express();
@@ -7,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1/order",orderRouter);
+app.use("/api/v1/depth",depthRouter);
+app.use("/api/v1/klines",klineRouter);
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
---
+++ exchange-backend/api/src/routes/kline.ts
@@ -0,0 +1,59 @@
+import { Client } from "pg";
+import dotenv from "dotenv"
+import { Request,Response, Router } from "express";
+
+dotenv.config();
+
+
+const pgClient = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'exchange',
+    password: process.env.DATABASE_PASSWORD,
+    port: 5432
+})
+
+pgClient.connect();
+
+export const klineRouter = Router();
+
+klineRouter.get("/", async (req:Request, res:Response) => {
+    const { market, interval, startTime, endTime } = req.query;
+
+    let query;
+    switch (interval) {
+        case '1m':
+            query = `SELECT * FROM klines_1m WHERE bucket >= $1 AND bucket <= $2`;
+            break;
+        case '1h':
+            query = `SELECT * FROM klines_1h WHERE bucket >= $1 AND bucket <= $2`;
+            break;
+        case '1w':
+            query = `SELECT * FROM klines_1w WHERE bucket >= $1 AND bucket <= $2`;
+            break;
+        default:
+            res.status(400).send('Invalid Interval');
+            return;
+    }
+
+    try {
+        //@ts-ignore
+        const result = await pgClient.query(query, [new Date(startTime * 1000 as string), new Date(endTime * 1000 as string)]);
+        res.json(result.rows.map(x => ({
+            close: x.close,
+            end: x.bucket,
+            high: x.high,
+            low: x.low,
+            open: x.open,
+            quoteVolume: x.quoteVolume,
+            start: x.start,
+            trades: x.trades,
+            volume: x.volume,
+        })))
+        return;
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+        return;
+    }
+})
