c399aed32197bdddefe2414c050928fb8bf6daf9
krishna <krishnashrivastava23@gmail.com>
2025-06-05 23:57:51 +0530

    feat: implement market trading interface with depth, ticker, and trade views

---
+++ exchange-frontend/app/components/TradeView.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from "react";
+import { ChartManager } from "../utils/ChartManager";
+import { KLine } from "../utils/types";
+import { getKlines } from "../utils/httpClient";
+
+export function TradeView({ market }: { market: string }) {
+    const chartRef = useRef<HTMLDivElement>(null);
+    const chartManagerRef = useRef<ChartManager>(null);
+    useEffect(() => {
+        const init = async () => {
+            let klineData: KLine[] = [];
+            try {
+                klineData = await getKlines(market, "1h", Math.floor((new Date().getTime() - 1000 * 60 * 60 * 24 * 7) / 1000), Math.floor(new Date().getTime() / 1000));
+            } catch (error) {
+
+            }
+            if (chartRef) {
+                if (chartManagerRef.current) {
+                    chartManagerRef.current.destroy();
+                }
+                console.log(klineData);
+                const chartManager = new ChartManager(
+                    chartRef.current,
+                    [
+                        ...klineData?.map((x) => ({
+                            close: parseFloat(x.close),
+                            high: parseFloat(x.high),
+                            low: parseFloat(x.low),
+                            open: parseFloat(x.open),
+                            timestamp: new Date(x.end),
+                        })),
+                    ].sort((x, y) => (x.timestamp < y.timestamp ? -1 : 1)) || [],
+                    {
+                        background: "#0e0f14",
+                        color: "white",
+                    }
+                );
+                //@ts-ignore
+                chartManagerRef.current = ChartManager;
+            }
+        };
+        init();
+    },[market,chartRef]);
+    return(
+        <>
+            <div ref={chartRef} style={{height: "520px", width: "100%", marginTop: 4}}></div>
+        </>
+    )
+}
--- exchange-frontend/app/utils/httpClient.ts
+++ exchange-frontend/app/utils/httpClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { Ticker } from "./types";
+import { Depth, KLine, Ticker, Trade} from "./types";
 
 
 const BASE_URL = "http://localhost:3005/api/v1";
@@ -19,4 +19,18 @@ export async function getTickers():Promise<Ticker[]>{
     return response.data;
 }
 
+export async function getKlines(market:string,interval:string,startTime:number,endTime:number):Promise<KLine[]>{
+    const response = await axios.get(`${BASE_URL}/klines?symbol=${market}&interval=${interval}&startTime=${startTime}&endTime=${endTime}`);
+    const data: KLine[] = response.data;
+    return data.sort((x,y)=>(Number(x.end)<Number(y.end)? -1 : 1));
+}
 
+export async function getDepth(market:string):Promise<Depth>{
+    const response = await axios.get(`${BASE_URL}/depth?symbol=${market}`);
+    return response.data;
+}
+
+export async function getTrades(market:string):Promise<Trade[]>{
+    const response = await axios.get(`${BASE_URL}/trades?symbol=${market}`);
+    return response.data;
+}
